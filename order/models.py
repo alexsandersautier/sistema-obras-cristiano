259ac86c24acf8c41d5435615d22b3a074b36f91
Alexander <alexsander@proxsis.net>
2025-07-31 20:04:59 -0300

    Add OrderItem model and update Order model with verbose names

--- order/models.py
+++ order/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from building.models import Building
+from service.models import ServicePrice
 
 # Create your models here.
 class Order(models.Model):
     
     building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='order_building', verbose_name='Obra')
-    start_date = models.DateField()
+    start_date = models.DateField(verbose_name='Início da obra')
     
     def __str__(self):
-        return f'Ordem de serviço referente a obra {self.building}'
+        return f'Ordem de serviço referente a obra {self.building} iniciada em {self.start_date}'
     
     class Meta:
         verbose_name = 'Ordem de serviço'
         verbose_name_plural = 'Ordens de serviço'    
+
+
+class OrderItem(models.Model):
+    
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitem_order', verbose_name='Ordem de serviço')
+    service_price = models.ForeignKey(ServicePrice, on_delete=models.CASCADE, related_name='orderitem_service', verbose_name='Serviço')
+    quantity = models.FloatField(verbose_name='Quantidade')
+    
+    def __str__(self):
+        return f'{self.service_price} da ordem de serviço {self.order}'
+    
+    class Meta:
+        verbose_name = 'Item da ordem de serviço'
+        verbose_name_plural = 'Itens da orden de serviço' 
