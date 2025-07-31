5735ad532900c05978c6e583508270b3874b3eb0
Alexander <alexsander@proxsis.net>
2025-07-31 20:18:27 -0300

    Refactor Team admin registration: add inline for TeamEmployee and customize TeamAdmin

--- team/admin.py
+++ team/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Team, TeamEmployee
 
-admin.site.register(Team)
-admin.site.register(TeamEmployee)
+class EmployeeInline(admin.TabularInline):
+    model = TeamEmployee
+    extra = 1
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [EmployeeInline]
+    
+admin.site.register(Team, TeamAdmin)
--- team/views.py
+++ team/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Team
-
-class TeamListView(ListView):
-    
-    queryset = Team.objects.all()
-    template_name = ''
