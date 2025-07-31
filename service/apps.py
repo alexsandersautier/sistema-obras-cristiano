64c223c8fad1eeed5540aed18a63abefdbf75e0e
Alexander <alexsander@proxsis.net>
2025-07-31 19:37:49 -0300

    Update verbose name for ServiceConfig to 'Gestão de serviços'

--- service/apps.py
+++ service/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'service'
-    verbose_name = 'Serviços'
+    verbose_name = 'Gestão de serviços'
