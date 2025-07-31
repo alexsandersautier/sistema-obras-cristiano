301993bf6d793c614b58d8a6db7fcf3065a54c73
Alexander <alexsander@proxsis.net>
2025-07-31 19:16:12 -0300

    Add Employee model and admin registration with verbose names

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    
+    name = models.CharField(verbose_name='Nome', max_length=255)
+    document_number = models.CharField(verbose_name='CPF/CNPJ', max_length=20)
+    phone_number = models.CharField(verbose_name='Telefone', max_length=11)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
