aa9a8e02feee1de39af2b1416d2a1973843e5529
Alexander <alexsander@proxsis.net>
2025-08-01 23:41:26 -0300

    feat: add footer with current year and developer information to custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,2 +1,5 @@
 const logoLink = document.getElementById('jazzy-logo');
 logoLink.setAttribute('href', '/admin/building/building/');
+
+const footer = document.querySelector('.main-footer')
+footer.innerHTML = `© ${new Date().getFullYear()} Sistema desenvolvido por Alexsander Sautier`
