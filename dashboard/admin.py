1f75cf6d0508737a6d690f7cbe132cfde5979ba6
= <=>
2025-08-03 22:01:14 -0300

    feat: enhance custom dashboard with dynamic filters and detailed order display

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.template.response import TemplateResponse
 from .models import FakeModel
-
 from django.shortcuts import redirect
 from django.urls import reverse
+
+from django.db.models.functions import ExtractYear
+from order.models import OrderItem
+from building.models import Building
+from team.models import Team
+from django.db.models import F, FloatField, ExpressionWrapper, Sum
 
 @admin.register(FakeModel)
 class CustomDashboardAdmin(admin.ModelAdmin):
@@ -24,10 +29,77 @@ class CustomDashboardAdmin(admin.ModelAdmin):
         return redirect(reverse("admin:custom-dashboard"))
 
     def dashboard_view(self, request):
+        years = OrderItem.objects.annotate(
+            year=ExtractYear('service_data')
+        ).values_list(
+            'year', flat=True
+        ).distinct().order_by('year')
+
+        buildings = Building.objects.filter(
+            order_building__orderitem_order__isnull=False
+        ).values_list('name', flat=True).distinct().order_by('name')
+
+        teams = Team.objects.all()
+
+        year = request.GET.get('year')  
+        month = request.GET.get('month')
+        building = request.GET.get('building')
+        team = request.GET.get('team')
+
+
+        orders = OrderItem.objects.select_related('service_price__service')
+
+        # aplicar filtros dinamicamente
+        if year and 'Todos' not in year:
+            orders = orders.filter(service_data__year=year)
+        if month and 'Todos' not in month:
+            orders = orders.filter(service_data__month=month)
+        if building and 'Todos' not in building:
+            orders = orders.filter(order__building__name=building)  # ou .name se você estiver passando o nome
+        if team and 'Todos' not in team:
+            orders = orders.filter(order__building__buildingteam_building__team__name=team)
+        
+        orders = orders.annotate(
+            total_service=ExpressionWrapper(
+                F('service_price__price') * F('quantity'),
+                output_field=FloatField()
+            )
+        )        
+        
+        total_geral = orders.aggregate(total=Sum('total_service'))['total'] or 0
+
+        MONTHS = [
+            ('1', 'Janeiro'),
+            ('2', 'Fevereiro'),
+            ('3', 'Março'),
+            ('4', 'Abril'),
+            ('5', 'Maio'),
+            ('6', 'Junho'),
+            ('7', 'Julho'),
+            ('8', 'Agosto'),
+            ('9', 'Setembro'),
+            ('10', 'Outubro'),
+            ('11', 'Novembro'),
+            ('12', 'Dezembro'),
+        ]
+
         context = dict(
             self.admin_site.each_context(request),
             title="Dashboard de Cálculos",
-            resultado={"soma": 123, "media": 456},
+            result={
+                "buildings": buildings, 
+                "orders": orders, 
+                "years": years, 
+                "teams": teams,
+            },
+            months=MONTHS,
+            total_geral=total_geral,
+            selected= {
+                "year": year,
+                "month": month,
+                "building": building,
+                "team": team,
+            }
         )
         return TemplateResponse(request, "admin/custom_dashboard.html", context)
 
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,35 @@
+const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth();
+
+const year = document.getElementById('yearSelect');
+const month = document.getElementById('monthSelect');
+const currentBuilding = document.getElementById('buildingSelect');
+const currentTeam = document.getElementById('teamSelect');
+
+document.getElementById('tableTitle').innerText = `Obra: ${currentBuilding.value} - Equipe: ${currentTeam.value}`
+
 const logoLink = document.getElementById('jazzy-logo');
 logoLink.setAttribute('href', '/admin/building/building/');
 
-const footer = document.querySelector('.main-footer')
-footer.innerHTML = `© ${new Date().getFullYear()} Sistema desenvolvido por Alexsander Sautier`
+const footer = document.querySelector('.main-footer');
+footer.innerHTML = `<strong>© ${currentYear} Sistema desenvolvido por Alexsander Sautier</strong>`;
+
+const filters = document.getElementById('formData');
+
+filters.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const params = new URLSearchParams({
+        year: year.value,
+        month: month.value,
+        building: currentBuilding.value,
+        team: currentTeam.value,
+    });
+
+    window.location.href = window.location.pathname + '?' + params.toString();
+});
+
+year.addEventListener('change', () => filters.requestSubmit());
+month.addEventListener('change', () => filters.requestSubmit());
+currentBuilding.addEventListener('change', () => filters.requestSubmit());
+currentTeam.addEventListener('change', () => filters.requestSubmit());
